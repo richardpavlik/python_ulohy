449d5fc3411d7fdc337bdfb491ee1a0b59933170
Richard Pavlík <richard.pavlik.rp@gmail.com>
2021-02-27 14:24:11 +0100

    úloha 1

---
+++ uloha1.py
@@ -0,0 +1,25 @@
+import math
+def kvadraticka():
+    a = int(input("Zadej první koeficient kvadratické rovnice: "))
+    b = int(input("Zadej druhý koeficient kvadratické rovnice: "))
+    c = int(input("Zadej třetí koeficient kvadratické rovnice: "))
+    diskriminant = b**2 - 4*a*c
+    return f"První kořen je {(-b + math.sqrt(diskriminant)) / (2*a)}, druhý kořen je {(-b - math.sqrt(diskriminant)) / (2*a)}"
+
+"""
+Verze s try a except
+
+import math
+def kvadraticka():
+    try:
+        a = int(input("Zadej první koeficient kvadratické rovnice: "))
+        b = int(input("Zadej druhý koeficient kvadratické rovnice: "))
+        c = int(input("Zadej třetí koeficient kvadratické rovnice: "))
+        diskriminant = b**2 - 4*a*c
+        return f"První kořen je {(-b + math.sqrt(diskriminant)) / (2*a)}, druhý kořen je {(-b - math.sqrt(diskriminant)) / (2*a)}"
+    except ValueError:
+        return "Zadej platné koeficienty kvadratické rovnice!"
+
+"""
+print(kvadraticka())
+
