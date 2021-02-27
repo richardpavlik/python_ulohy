d99bf0a184d4c596b12bceb555021e7429b6a243
Richard Pavlík <richard.pavlik.rp@gmail.com>
2021-02-27 13:30:22 +0100

    2.1 dělení nulou

---
+++ 2.1.py
@@ -0,0 +1,17 @@
+a = int(input("Zadej číslo a: "))
+b = int(input("Zadej číslo b: "))
+op = input("Vyber operátor z nabídky +, -, *, / :   ")
+if b == 0 and op == "/":
+    print("Nemůžeš dělit nulou!\nZadej buď jiné číslo nebo operátor!")
+    b = int(input("Zadej číslo b: "))
+print("Zvolil jsi operaci",op)
+if op == "+":   
+    vysl = a + b
+elif op == "-": 
+    vysl = a - b
+elif op == "*": 
+    vysl = a * b
+else:   
+    vysl = a/b
+print(a, op, b, "=", vysl)
+
