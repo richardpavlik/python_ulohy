721f53267ca9dd08b4e4fbe91c7de0b1899bfbc6
Richard Pavlík <richard.pavlik.rp@gmail.com>
2021-02-28 08:31:23 +0100

    Úloha 1

--- uloha1.py
+++ uloha1.py
@@ -1,11 +1,15 @@
 import math
 def kvadraticka():
-    a = int(input("Zadej první koeficient kvadratické rovnice: "))
-    b = int(input("Zadej druhý koeficient kvadratické rovnice: "))
-    c = int(input("Zadej třetí koeficient kvadratické rovnice: "))
+    a = float((input("Zadej první koeficient kvadratické rovnice: ")))
+    b = float((input("Zadej druhý koeficient kvadratické rovnice: ")))
+    c = float((input("Zadej třetí koeficient kvadratické rovnice: ")))
+    if a == 0 or b == 0 or c == 0:
+        return "Koeficient se nesmí rovnat nule!"
     diskriminant = b**2 - 4*a*c
     if diskriminant < 0 :
-        return None
+        return "Dikriminant je menší než nula, zadej jiné koeficienty."
+    print(f"Dikriminant je: {diskriminant}")
+    print(f"Kvadratická rovnice vypadá takto: {a}x2 + {b}x + {c}")
     return f"První kořen je {(-b + math.sqrt(diskriminant)) / (2*a)}, druhý kořen je {(-b - math.sqrt(diskriminant)) / (2*a)}"
 
 """
