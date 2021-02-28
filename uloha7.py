be198d575a75872ae8f4aadbe742cc4feca5cf66
Richard Pavlík <richard.pavlik.rp@gmail.com>
2021-02-28 08:16:52 +0100

    úloha 7

--- uloha7.py
+++ uloha7.py
@@ -1,6 +1,7 @@
 import random
 def nahodne_cislo():
     tajne_cislo = random.choice(range(1, 10))
+    print(tajne_cislo)
     print("Zkus uhádnout číslo od 1 do 9, máš na to pouze tři pokusy!")
     pokusy = 3
     while pokusy > 0:
@@ -11,8 +12,7 @@ def nahodne_cislo():
         if pokusy > 0:
             print(f"Ještě máš tolik pokusů: {pokusy}")
         else:
-            print("Prohrál/a si!")
-
+            return "Prohrál/a si!"
 
 #verze s try except - když hráč zadá něco jiného než číslo, program nespadne, ale vypíše zprávu a spustí se znova
 """
